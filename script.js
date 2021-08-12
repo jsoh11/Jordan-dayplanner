c755a374ed3c3215a95a34b9dbb4237cf0d4821e
Jordan <jordansoh14@gmail.com>
2021-08-12 13:28:07 -0500

    update .js

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
-let currentDate = moment().format("dddd") + "" + moment().format("Do MMM YYYY");
-let currentHour = moment().format('h:mm:ss a');
+// let currentDate = moment().format("dddd") + "" + moment().format("Do MMM YYYY");
+// let currentHour = moment().format('h:mm:ss a');
 
-let interval = setInterval(function) {
-let timeNow = moment();
-$('#currentDay').html(timeNow.format("YYYY MMMM DD")) + " "
-+ timeNow.format("dddd")
-.substring(0.3).toUpperCase();
-
+function getHeaderDate() {
+    let currentHeaderDate = moment().format("MMM Do YYYY H:ss");
+    $("#currentDay").text(currentHeaderDate);
 }
+getHeaderDate()
 
-let nineHour = $('#hour-9');
-let tenHour = $('#hour-10');
-let elevenHour = $('#hour-11');
-let twelveHour = $('#hour-12');
-let thirteenHour = $('#hour-13');
-let fourteenHour = $('#hour-14');
-let fifteenHour = $('#hour-15');
-let sixteenHour = $('#hour-16');
-let seventeenHour = $('#hour-17');
+let nineHour = $('#hour9');
 
+let tenHour = $('#hour10');
+
+let elevenHour = $('#hour11');
+
+let twelveHour = $('#hour12');
+
+let thirteenHour = $('#hour13');
+
+let fourteenHour = $('#hour14');
+
+let fifteenHour = $('#hour15');
+
+let sixteenHour = $('#hour16');
+
+let seventeenHour = $('#hour17');
+
+let myHour = [
+    {nineHour},
+
+    {tenHour},
+
+    {elevenHour},
+
+    {twelveHour},
+
+    {thirteenHour},
+
+    {fourteenHour},
+
+    {fifteenHour},
+
+    {sixteenHour},
+
+    {seventeenHour},
+
+]
 
 
 
