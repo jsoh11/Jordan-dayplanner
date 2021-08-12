2b5ccdca69b6c7cedd7ccb91af8f569c2c6dca6d
Jordan <jordansoh14@gmail.com>
2021-08-11 19:11:31 -0500

    updated .js

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 let today = moment();
-$("#currentDay").text(today.format("MMM Do, YYYY H:HH"));
+$("#currentDay").text(today.format("MMM Do, YYYY, H:mm"));
+$timeBlock = $('time-block');
+
+let now =  moment().format("H HH")
+
+function rowColor() {
+
+    if (hour) {
+        $timeBlock > now
+        $timeBlock.css("background-color", "lightgray")
+    }
+}
