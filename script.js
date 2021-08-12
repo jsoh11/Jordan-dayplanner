60843ca80dabb6c42bb8b603cde6327c937c078d
Jordan <jordansoh14@gmail.com>
2021-08-12 13:03:05 -0500

    updated .js

--- script.js
+++ script.js
@@ -1,13 +1,57 @@
-let today = moment();
-$("#currentDay").text(today.format("MMM Do, YYYY, H:mm"));
-$timeBlock = $('time-block');
+let currentDate = moment().format("dddd") + "" + moment().format("Do MMM YYYY");
+let currentHour = moment().format('h:mm:ss a');
 
-let now =  moment().format("H HH")
+let interval = setInterval(function) {
+let timeNow = moment();
+$('#currentDay').html(timeNow.format("YYYY MMMM DD")) + " "
++ timeNow.format("dddd")
+.substring(0.3).toUpperCase();
 
-function rowColor() {
-
-    if (hour) {
-        $timeBlock > now
-        $timeBlock.css("background-color", "lightgray")
-    }
 }
+
+let nineHour = $('#hour-9');
+let tenHour = $('#hour-10');
+let elevenHour = $('#hour-11');
+let twelveHour = $('#hour-12');
+let thirteenHour = $('#hour-13');
+let fourteenHour = $('#hour-14');
+let fifteenHour = $('#hour-15');
+let sixteenHour = $('#hour-16');
+let seventeenHour = $('#hour-17');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let today = moment();
+// $("#currentDay").text(today.format("MMM Do, YYYY, H:mm"));
